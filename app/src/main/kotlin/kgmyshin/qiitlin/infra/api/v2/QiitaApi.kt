81332948f94f17e143dee07f765b627f952664cd
釘宮愼之介 <kgmyshin@kugimiyasuke-no-MacBook-Air.local>
2015-05-03 16:24:21 +0900

    [add] 1apiでの取得数を変更

--- app/src/main/kotlin/kgmyshin/qiitlin/infra/api/v2/QiitaApi.kt
+++ app/src/main/kotlin/kgmyshin/qiitlin/infra/api/v2/QiitaApi.kt
@@ -12,21 +12,21 @@ import retrofit.http.Query
  */
 public trait QiitaApi {
 
-    [GET("/items")]
+    [GET("/items?per_page=100")]
     fun getArticles() : List<Article>
 
-    [GET("/items")]
+    [GET("/items?per_page=100")]
     fun getArticles([Query("page")] page:Int) : List<Article>
 
-    [GET("/items/{item_id}/comments")]
+    [GET("/items/{item_id}/comments?per_page=100")]
     fun getComments([Path("item_id")] itemId: String) : List<Comment>
 
-    [GET("/items/{item_id}/comments")]
+    [GET("/items/{item_id}/comments?per_page=100")]
     fun getComments([Path("item_id")] itemId: String, [Query("page")] page:Int) : List<Comment>
 
-    [GET("/tags")]
+    [GET("/tags?per_page=100")]
     fun getTags() : List<Tag>
 
-    [GET("/tags")]
+    [GET("/tags?per_page=100")]
     fun getTags([Query("page")] page:Int) : List<Tag>
 }
